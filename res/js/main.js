7f1b86347a1fa2d5df5c356ac5c39270adfc4e56
filipekxd <sakrfilip@seznam.cz>
2023-10-06 08:26:53 +0200

    init

---
+++ res/js/main.js
@@ -0,0 +1,14 @@
+const draven = document.getElementById("draven");
+const counter = document.getElementById("counter");
+
+let numberOfDravens = 0;
+//.onclick - na kliknuti
+// () => arrow funkce
+// {} - scope
+draven.onclick = () => {
+    //zvednout cislo o 1
+numberOfDravens += 1;
+
+    //zobrazit v odstavci
+    counter.innerText = "dravens:" + numberOfDravens;
+}  
